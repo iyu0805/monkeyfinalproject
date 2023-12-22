572f4d781aa5e8ab8734dda3d87dba7794fd829e
iyu0805 <t110360142@ntut.org.tw>
2023-12-22 14:59:27 +0800

    model

---
+++ monkeyfinalproject/app/src/main/java/com/example/monkeyfinalproject/startmodel.java
@@ -0,0 +1,14 @@
+package com.example.monkeyfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startmodel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startmodel);
+    }
+}
